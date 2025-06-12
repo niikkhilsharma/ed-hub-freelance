50f832c5a9c7b0e09098e6591ab354382d01da8b
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2025-06-13 01:03:37 +0530

    WIP: Save local changes before pull

---
+++ app/admin/layout.tsx
@@ -0,0 +1,25 @@
+import MaxWidthWrapper from '@/components/admin/max-width-wrapper'
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+import { Poppins } from 'next/font/google'
+
+const poppinsFont = Poppins({
+	style: 'normal',
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-poppins',
+})
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<div className={poppinsFont.className}>
+			<Header user={{ name: 'Nikhil', role: 'Student', avatarSrc: '/page3/entry/pri.png' }} isAskme={false} />
+			<MaxWidthWrapper className="px-4 sm:px-20">{children}</MaxWidthWrapper>
+			<Footer />
+		</div>
+	)
+}
