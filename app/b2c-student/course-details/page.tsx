fb054cc144c001de5898fd86292c0f2e5fd7dde6
tanushka <tanushka1726@gmail.com>
2025-06-15 01:43:11 +0530

    edit b2c-student images

---
+++ app/b2c-student/course-details/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import GoBack from "@/components/principal/goback";
+
+
+const coursedetail = () =>{
+    return(
+        <>
+        <GoBack GoBackHeading="Course Name" />
+        <img src="/b2c-student" alt="" width={200} height={200}/>
+
+        </>
+    )
+}
+
+export default coursedetail;
---
+++ app/b2c-student/teacher-profile/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const teacherprofile = ()=>{
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default teacherprofile;
