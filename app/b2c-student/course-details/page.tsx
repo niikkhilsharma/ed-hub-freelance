bbaa97236ae20b0b039e393cc00d7f035d1c486e
HevenDev <hevendev@gmail.com>
2025-06-15 08:36:31 +0530

    b2c-student page structure

--- app/b2c-student/course-details/page.tsx
+++ app/b2c-student/course-details/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import GoBack from "@/components/principal/goback";
+import Course from "@/components/b2c-student/course";
 
 
 const coursedetail = () =>{
     return(
         <>
         <GoBack GoBackHeading="Course Name" />
-
+        <Course />
 
         </>
     )
