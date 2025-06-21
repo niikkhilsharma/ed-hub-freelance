95c613369254654d274574b3aa492baa9415c5c4
tanushka1726 <tanushka1726@gmail.com>
2025-06-21 17:00:16 +0000

    b2c-student 10/10 pages done

---
+++ app/b2c-student/student-flow/material-page-course/page.tsx
@@ -0,0 +1,7 @@
+import MaterialPageCourse from "@/components/b2c-student/material-page-course";
+
+export default function CourseDetail(){
+    return <div>
+        <MaterialPageCourse /> 
+    </div>
+}
