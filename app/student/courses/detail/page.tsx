ae8ed40f07140281b7102eb0383ab0f6ab9695c1
Utkarsh <utka21200@gmail.com>
2025-06-23 00:39:25 +0530

    feat: added video cta button on course detail page

--- app/student/courses/detail/page.tsx
+++ app/student/courses/detail/page.tsx
@@ -523,7 +523,7 @@ Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed 
 
         <div className="relative flex bg-[#FFFFFF] rounded-2xl mx-auto overflow-hidden">
           {/* Fixed Sidebar */}
-          <button className="bg-[#8DD9B3] flex items-center gap-3 px-4 py-2 text-white text-left rounded-2xl absolute top-5 right-6 cursor-pointer">
+          <button className="bg-[#8DD9B3] flex items-center gap-1 px-3 py-2 text-white text-left rounded-2xl absolute top-5 right-6 cursor-pointer">
             <Video />
             Watch Demo Video
           </button>
