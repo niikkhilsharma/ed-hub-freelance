ff854b88caa00efe52a8a6c8255b60adff4de35b
shivani <shivani11guleria@gmail.com>
2025-07-01 13:44:24 +0530

    b2c admin2

---
+++ app/admin-b2c/admin-panel/course-management/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import CourseManagementPage from "@/components/b2c-admin/course-management"
+export default function  page  () {
+  return (
+    <div>
+        < CourseManagementPage/>
+    </div>
+  )
+}
