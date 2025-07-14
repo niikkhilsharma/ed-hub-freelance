560653e5e4e4e56b553341bcfed1858ed6a5518d
tanushka1726 <tanushka1726@gmail.com>
2025-07-14 13:49:21 +0000

    Fixed filter layout shift

--- components/b2c-admin/common-component/SearchBarFilter.tsx
+++ components/b2c-admin/common-component/SearchBarFilter.tsx
@@ -24,23 +24,30 @@ const Filters: FC<FiltersProps> = ({ filters }) => {
       {filters.map((filter) => {
         const isOpen = openFilterId === filter.id;
         return (
-          <div key={filter.id} className="relative whitespace-nowrap bg-[#f9fafb] border rounded-xl w-fit">
-            <button
-              onClick={() => toggleDropdown(filter.id)}
-              className="text-xs sm:text-sm px-3 py-2 cursor-pointer flex items-center gap-2 w-full"
-            >
-              {filter.label}
-              {isOpen ? <FiChevronUp size={14} /> : <FiChevronDown size={14} />}
-            </button>
+          <div key={filter.id} className="relative inline-block text-left">
+            {/* Button + border container */}
+            <div className={`bg-[#f9fafb]  ${isOpen ? "rounded-t-xl border-t border-x" : "rounded-xl border"} box-border`}>
+              <button
+                onClick={() => toggleDropdown(filter.id)}
+                className="text-xs sm:text-sm px-3 py-2 cursor-pointer flex items-center gap-2 w-full"
+              >
+                {filter.label}
+                {isOpen ? <FiChevronUp size={14} /> : <FiChevronDown size={14} />}
+              </button>
+            </div>
 
+            {/* Dropdown */}
             {isOpen && (
-              <div className="text-xs sm:text-sm">
-                <div className="px-4 py-2 text-gray-500 cursor-pointer">Option 1</div>
-                <div className="px-4 py-2 text-gray-500 cursor-pointer">Option 2</div>
+              <div className="absolute left-0 top-full w-full bg-[#f9fafb] border-x border-b rounded-b-xl z-10 box-border">
+                <button className="whitespace-nowrap justify-center py-2 w-full flex items-center text-gray-500 cursor-pointer">
+                  Option 1
+                </button>
+                <button className="whitespace-nowrap justify-center py-2 w-full flex items-center text-gray-500 cursor-pointer">
+                  Option 2
+                </button>
               </div>
             )}
           </div>
-
         );
       })}
     </div>
@@ -50,11 +57,13 @@ const Filters: FC<FiltersProps> = ({ filters }) => {
 // Main SearchFilter component
 interface SearchFilterProps {
   filters: Filter[];
+  bg?: String;
 }
 
-const SearchFilter: FC<SearchFilterProps> = ({ filters }) => {
+const SearchFilter: FC<SearchFilterProps> = ({ filters, bg }) => {
   return (
-    <div className="w-full bg-[#f9fafb] text-black flex flex-col sm:flex-row gap-4 items-center py-2 rounded-xl">
+    <div className={`w-full text-black flex flex-col sm:flex-row gap-4 items-center py-2 rounded-xl ${bg ? bg : "bg-[#f9fafb]"
+      }`}>
       {/* Search Input */}
       <div className="flex items-center w-full sm:w-auto flex-grow border-2 border-[#6B7280] rounded-full px-3 py-2 focus-within:ring-2 focus-within:ring-gray-400">
         <FiSearch size={20} className="text-black mr-2" />
