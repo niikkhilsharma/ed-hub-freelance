8a0b5a0d813229ce2399a42ab5ae43a0a2043575
tanushka1726 <tanushka1726@gmail.com>
2025-07-15 13:41:35 +0000

    popup teacher b2c

---
+++ app/b2c-teacher/ct-pop-ups/page.tsx
@@ -0,0 +1,53 @@
+
+"use client";
+
+import React, { useState, useEffect } from "react";
+import EditDemoVideo from "./popupComponent/EditVideoDemo";
+import Popup from "./popupComponent/Popup";
+
+
+
+
+// --- Main Page Component to trigger modals ---
+export default function AllTeacherB2CPopups() {
+    const [openModal, setOpenModal] = useState<string | null>(null);
+    // const [showActionsPopup, setShowActionsPopup] = useState(false);
+
+    const modalButtons = [
+        { id: "editDemoVideo", label: "Edit Demo Video" },
+        { id: "popup", label: "Popup" },
+        // here you can add pop id and it's label to show it on the page 
+    ];
+
+    return (
+        <div className="bg-gray-200 min-h-screen p-10 font-sans">
+            <h1 className="text-3xl font-bold text-center mb-10 text-[#6B7280]">
+                C&T Teacher B2C Popups
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {modalButtons.map((btn) => (
+                    <button
+                        key={btn.id}
+                        onClick={() => setOpenModal(btn.id)}
+                        className="bg-white text-[#6B7280] font-semibold py-3 px-4 rounded-lg shadow-md transition-all hover:shadow-lg hover:-translate-y-0.5"
+                    >
+                        {btn.label}
+                    </button>
+                ))}
+            </div>
+
+            {/* Edit Demo Video pop  */}
+            <EditDemoVideo
+                isOpen={openModal === "editDemoVideo"}
+                onClose={() => setOpenModal(null)}
+            />
+            
+            {/* Pop up  */}
+            <Popup
+                isOpen={openModal === "popup"}
+                onClose={() => setOpenModal(null)}
+            />
+            
+        </div>
+    );
+}
---
+++ app/b2c-teacher/ct-pop-ups/popupComponent/EditVideoDemo.tsx
@@ -0,0 +1,35 @@
+import { FiX } from "react-icons/fi";
+import { PopupPropB2CTeacher, TeacherB2CBaseModal } from "../page";
+import { IoCloudUploadOutline } from "react-icons/io5";
+
+const EditDemoVideo: React.FC<PopupPropB2CTeacher> = ({
+    isOpen,
+    onClose,
+}) => {
+    return (
+        <TeacherB2CBaseModal isOpen={isOpen} onClose={onClose} maxWidth="max-w-md">
+            <div className="rounded-3xl px-4 pt-6 pb-4 ">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-base font-semibold">EditDemoVideo</h2>
+                    <button
+                        onClick={onClose}
+                        className="p-1.5 bg-[#F9FAFB] rounded-full hover:bg-gray-200"
+                    >
+                        <FiX size={25}/>
+                    </button>
+                </div>
+                <div className="mt-8">
+                    <label className=' text-base'>Upload Document</label>
+                    <div className="flex items-center gap-2 rounded-full my-2 border border-gray-300 bg-gray-50 p-2 text-gray-400">
+                        <div className="p-1 bg-pink-100 rounded-full">
+                            <IoCloudUploadOutline className="text-[#FF3366]" size={20} />
+                        </div>
+                        <span className="truncate">Document Name</span>
+                    </div>
+                </div>
+            </div>
+        </TeacherB2CBaseModal>
+    );
+};
+
+export default EditDemoVideo;
